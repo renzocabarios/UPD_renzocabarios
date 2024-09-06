9b35d11227ca5f8feef288a624a6ee58a2f8943c
renzocabarios <renzo.cabarios@gmail.com>
2024-09-06 18:46:32 +0800

    Update

---
+++ transfer_and_create.ts
@@ -0,0 +1,47 @@
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import bs58 from "bs58";
+
+(async () => {
+  const CONNECTION = new Connection(clusterApiUrl("devnet"));
+
+  const PRIVATE_KEY =
+    "5gQWXvxghHYQKLm2F4vAa4JKE87YdcynKrkWT2AuheFLDKS3EAjLDWZHUkVmkov3gNRQLh6p6VSRvfc9HGjEHcZn";
+
+  const decoded = bs58.decode(PRIVATE_KEY);
+  const NEW_ACCOUNT_KEYPAIR = Keypair.generate();
+
+  const MY_KEYPAIR = Keypair.fromSecretKey(decoded);
+
+  const tx = new Transaction();
+  const lamports = await CONNECTION.getMinimumBalanceForRentExemption(0);
+
+  tx.instructions = [
+    SystemProgram.transfer({
+      fromPubkey: MY_KEYPAIR.publicKey,
+      toPubkey: new PublicKey("BNmMoLvMnCWgZE8vrf9NUTatZv3vZoYcz34oyMSf8vVk"),
+      lamports,
+      programId: SystemProgram.programId,
+    }),
+    SystemProgram.createAccount({
+      fromPubkey: MY_KEYPAIR.publicKey,
+      newAccountPubkey: NEW_ACCOUNT_KEYPAIR.publicKey,
+      lamports,
+      space: 0,
+      programId: SystemProgram.programId,
+    }),
+  ];
+
+  const txHash = await CONNECTION.sendTransaction(tx, [
+    MY_KEYPAIR,
+    NEW_ACCOUNT_KEYPAIR,
+  ]);
+
+  console.log(txHash);
+})();
