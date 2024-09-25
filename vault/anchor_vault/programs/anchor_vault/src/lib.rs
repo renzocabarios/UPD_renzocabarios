470188e2cbced37f535160d9002aa29fa656c377
renzocabarios <renzo.cabarios@gmail.com>
2024-09-25 18:32:06 +0800

    Update

--- vault/anchor_vault/programs/anchor_vault/src/lib.rs
+++ vault/anchor_vault/programs/anchor_vault/src/lib.rs
@@ -1,16 +1,54 @@
 use anchor_lang::prelude::*;
-
+use anchor_lang::system_program::transfer;
+use anchor_lang::system_program::Transfer;
 declare_id!("39q87wUrAWA1LKiUtD7eiZdKJ6DhuoB5jVAznpuEqy1t");
 
 #[program]
 pub mod anchor_vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn deposit(ctx: Context<Vault>, amount: u64) -> Result<()> {
+        ctx.accounts.deposit(amount)
+    }
+    pub fn withdraw(ctx: Context<Vault>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, &[ctx.bumps.vault])
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Vault<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut, seeds = [signer.key().as_ref()], bump)]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Vault<'info> {
+    pub fn deposit(&mut self, amount: u64) -> Result<()> {
+        let accounts = Transfer {
+            from: self.signer.to_account_info(),
+            to: self.vault.to_account_info(),
+        };
+
+        let ctx = CpiContext::new(self.system_program.to_account_info(), accounts);
+
+        transfer(ctx, amount)
+    }
+
+    pub fn withdraw(&mut self, amount: u64, bump: &[u8]) -> Result<()> {
+        let signer_seeds = [&[self.signer.to_account_info().key.as_ref(), bump][..]];
+        let accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.signer.to_account_info(),
+        };
+
+        let ctx = CpiContext::new_with_signer(
+            self.system_program.to_account_info(),
+            accounts,
+            &signer_seeds,
+        );
+
+        transfer(ctx, amount)
+    }
+}
--- vault/anchor_vault/tests/anchor_vault.ts
+++ vault/anchor_vault/tests/anchor_vault.ts
@@ -1,16 +1,26 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { BN, Program } from "@coral-xyz/anchor";
 import { AnchorVault } from "../target/types/anchor_vault";
 
 describe("anchor_vault", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.getProvider();
   const program = anchor.workspace.AnchorVault as Program<AnchorVault>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Deposit", async () => {
+    const tx = await program.methods
+      .deposit(new BN(1_000_000))
+      .accounts({ signer: provider.publicKey! })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Withdraw", async () => {
+    const tx = await program.methods
+      .withdraw(new BN(1_000_000))
+      .accounts({ signer: provider.publicKey! })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
